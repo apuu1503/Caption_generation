35e6cd59b0237f52d0cb9efc231ab3b469fa34e3
apuu1503 <apurvasuakle2003@gmail.com>
2024-05-29 23:28:19 +0530

    template1

--- src/forms/PersonalInformationForm.jsx
+++ src/forms/PersonalInformationForm.jsx
@@ -61,6 +61,23 @@ function PersonalInformationForm({ formData, handleChange }) {
                             </div>
                         </div>
                         <div className="sm:col-span-4">
+                            <label htmlFor="contact" className="block text-sm font-medium leading-6 text-gray-900">
+                               Contact number
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="contact"
+                                    name="contact"
+                                    type="contact"
+                                    value={formData.contact}
+                                    onChange={handleChange}
+                                    autoComplete="email"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <div className="sm:col-span-4">
                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                                 Email address
                             </label>
